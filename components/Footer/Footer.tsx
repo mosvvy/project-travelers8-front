0f6a82ef0ed7436591f4ee09ee492ce091d5998f
ValeriiaM <moskalenkovvy@gmail.com>
2026-02-20 14:03:08 +0200

    add modern-normalize, Header< Footer, Modal components files

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,7 @@
+import css from './Footer.module.css';
+
+const Footer = () => {
+  return <footer>Footer</footer>;
+};
+
+export default Footer;
---
+++ components/Modal/Modal.tsx
@@ -0,0 +1,7 @@
+import css from './Modal.module.css';
+
+const Modal = () => {
+  return Modal;
+};
+
+export default Modal;
