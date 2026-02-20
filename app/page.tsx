23545ffb978fef602465c6809d3fcf8b0807d617
ValeriiaM <moskalenkovvy@gmail.com>
2026-02-20 19:43:32 +0200

    add pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import styles from './page.module.css';
+import css from './page.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}></main>
-    </div>
+    <>
+      <h1>Головна</h1>
+      <h2>project</h2>
+      <h2>popular stories</h2>
+      <h2>travelers</h2>
+      <h2>join us</h2>
+    </>
   );
 }
