7a2473508ca79ca9ec6951006d80f8d588801310
Liliia Dudnyk <91532622+LiliiaDud@users.noreply.github.com>
2026-02-24 00:54:48 +0100

    add Hero component to the home page

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,7 +1,33 @@
 import css from './Hero.module.css';
+import Button from '../Button/Button';
+//import Section from '../Section/Section';
 
 const Hero = () => {
-  return <h1>Hero</h1>;
+  return (
+    <Section className={css.hero}>
+      <div className={css.heroBackground}>
+        <div className={css.content}>
+          <h1 className={css.heroTitle}>
+            Відкрийте світ подорожей з нами!
+          </h1>
+          <p className={css.heroDescription}>
+            Приєднуйтесь до нашої спільноти мандрівників, де ви зможете ділитися
+            своїми історіями та отримувати натхнення для нових пригод. Відкрийте
+            для себе нові місця та знайдіть однодумців!
+          </p>
+          <Button
+            href="#join"
+            variant="blue"
+            fullWidth
+            size="medium"
+            className={heroButton}
+          >
+            Доєднатись
+          </Button>
+        </div>
+      </div>
+    </Section>
+  );
 };
 
 export default Hero;
