8169dce66a4b8f6940d6a792b34893de2e031b58
Liliia Dudnyk <91532622+LiliiaDud@users.noreply.github.com>
2026-02-24 14:17:11 +0100

    refactor Hero,clean redundant button styles and improve responsive widths.

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,31 +1,29 @@
+'use client';
+
 import css from './Hero.module.css';
 import Button from '../Button/Button';
-//import Section from '../Section/Section';
+import Section from '../Section/Section';
 
 const Hero = () => {
   return (
-    <Section className={css.hero}>
-      <div className={css.heroBackground}>
-        <div className={css.content}>
-          <h1 className={css.heroTitle}>
-            Відкрийте світ подорожей з нами!
-          </h1>
-          <p className={css.heroDescription}>
-            Приєднуйтесь до нашої спільноти мандрівників, де ви зможете ділитися
-            своїми історіями та отримувати натхнення для нових пригод. Відкрийте
-            для себе нові місця та знайдіть однодумців!
-          </p>
-          <Button
-            href="#join"
-            variant="blue"
-            fullWidth
-            size="medium"
-            className={heroButton}
-          >
-            Доєднатись
-          </Button>
-        </div>
-      </div>
+   <Section 
+      sectionClassName={css.hero} 
+      containerClassName={css.heroContainer}>
+      <h1 className={css.heroTitle}>
+        Відкрийте світ подорожей з нами!
+      </h1>
+      <p className={css.heroDescription}>
+        Приєднуйтесь до нашої спільноти мандрівників, де ви зможете ділитися
+        своїми історіями та отримувати натхнення для нових пригод. Відкрийте
+        для себе нові місця та знайдіть однодумців!
+      </p>
+      <Button 
+        href="#join" 
+        variant="primary"
+        className={css.heroButton}
+      >
+        Доєднатись
+      </Button>
     </Section>
   );
 };
