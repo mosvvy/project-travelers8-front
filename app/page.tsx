d176298a2297808fd7cd80217ba912b744738999
Liliia Dudnyk <91532622+LiliiaDud@users.noreply.github.com>
2026-02-24 01:55:42 +0100

    connect Hero to page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Hero from '@/components/Hero/Hero';
 import css from './page.module.css';
 
 export default function HomePage() {
   return (
     <>
       <h1>Головна</h1>
+      <Hero/ >
       <h2>project</h2>
       <h2>popular stories</h2>
       <h2>travelers</h2>
