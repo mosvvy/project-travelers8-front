eecf4c21a0497af05c21836dd643eb68d71077b4
Iryna Yeremizina <yeremizina1@gmail.com>
2026-02-25 18:51:23 +0200

    виклик компонента <About />

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import css from './page.module.css';
+import About from '../components/About/About';
 
 export default function HomePage() {
   return (
     <>
       <h1>Головна</h1>
-      <h2>project</h2>
+      <About />
       <h2>popular stories</h2>
       <h2>travelers</h2>
       <h2>join us</h2>
