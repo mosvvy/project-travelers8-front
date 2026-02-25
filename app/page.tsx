d3be89020708eea164d7514b0adea9a32d7c1931
Liliia Dudnyk <91532622+LiliiaDud@users.noreply.github.com>
2026-02-25 17:18:00 +0100

    removed "<h1>Головна</h1>"

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,7 @@ import css from './page.module.css';
 export default function HomePage() {
   return (
     <>
-      <h1>Головна</h1>
-      <Hero/ >
+      <Hero />
       <h2>project</h2>
       <h2>popular stories</h2>
       <h2>travelers</h2>
