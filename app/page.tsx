f5051e5b2a103c075e3f15b6ae2b250ecf02f391
Liliia Dudnyk <91532622+LiliiaDud@users.noreply.github.com>
2026-02-25 16:59:03 +0100

    feat(join): add responsive layout and background image-set

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,16 @@
 import Hero from '@/components/Hero/Hero';
+import Join from '@/components/Join/Join';  
 import css from './page.module.css';
 
 export default function HomePage() {
   return (
     <>
       <h1>Головна</h1>
-      <Hero/ >
+      <Hero />
       <h2>project</h2>
       <h2>popular stories</h2>
-      <h2>travelers</h2>
-      <h2>join us</h2>
+      <h2>travelers</h2>s
+       <Join />
     </>
   );
 }
--- components/Join/Join.tsx
+++ components/Join/Join.tsx
@@ -1,7 +1,37 @@
+'use client';
 import css from './Join.module.css';
+import Link from '../Link/Link';
+import Section from '../Section/Section';
 
-const Join = () => {
-  return <h2>Join</h2>;
-};
-
-export default Join;
+export default function Join() {
+  // const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
+  return (
+     <Section 
+      sectionClassName={css.join} containerClassName={css.joinContainer} id="join"  > 
+        <div className={css.joinBackground}>
+          <div className={css.joinContent}>
+            <h2 className={css.joinTitle}>Приєднуйтесь до нашої спільноти</h2>
+            <p className={css.joinDescription}>
+              Долучайтеся до мандрівників, які діляться своїми історіями та
+              надихають на нові пригоди.
+            </p>
+      
+            {/* <Link
+                  variant="primaryBtn"
+                  href={isLoggedIn ? "/profile" : "/auth/register"}
+                  className={css.joinButton}
+                >
+                  {isLoggedIn ? "Збережені" : "Зареєструватися"}
+                </Link> */}
+            <Link
+              variant="primaryBtn"
+              href="/auth/register"
+              className={css.joinButton}
+            >
+            Зареєструватися
+            </Link>
+          </div>
+      </div>
+    </Section>
+  );
+}
