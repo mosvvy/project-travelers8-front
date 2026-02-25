beab2a3439968659d14d5fa3eb722bfd006bd998
ValeriiaM <moskalenkovvy@gmail.com>
2026-02-25 19:54:30 +0200

    move id to st div

--- components/Join/Join.tsx
+++ components/Join/Join.tsx
@@ -6,31 +6,25 @@ import Section from '../Section/Section';
 export default function Join() {
   // const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
   return (
-     <Section 
-      sectionClassName={css.join} containerClassName={css.joinContainer} id="join"  > 
-        <div className={css.joinBackground}>
-          <div className={css.joinContent}>
-            <h2 className={css.joinTitle}>Приєднуйтесь до нашої спільноти</h2>
-            <p className={css.joinDescription}>
-              Долучайтеся до мандрівників, які діляться своїми історіями та
-              надихають на нові пригоди.
-            </p>
-      
-            {/* <Link
+    <Section sectionClassName={css.join} containerClassName={css.joinContainer}>
+      <div id='join' className={css.joinBackground}>
+        <div className={css.joinContent}>
+          <h2 className={css.joinTitle}>Приєднуйтесь до нашої спільноти</h2>
+          <p className={css.joinDescription}>
+            Долучайтеся до мандрівників, які діляться своїми історіями та надихають на нові пригоди.
+          </p>
+
+          {/* <Link
                   variant="primaryBtn"
                   href={isLoggedIn ? "/profile" : "/auth/register"}
                   className={css.joinButton}
                 >
                   {isLoggedIn ? "Збережені" : "Зареєструватися"}
                 </Link> */}
-            <Link
-              variant="primaryBtn"
-              href="/auth/register"
-              className={css.joinButton}
-            >
+          <Link variant='primaryBtn' href='/auth/register' className={css.joinButton}>
             Зареєструватися
-            </Link>
-          </div>
+          </Link>
+        </div>
       </div>
     </Section>
   );
