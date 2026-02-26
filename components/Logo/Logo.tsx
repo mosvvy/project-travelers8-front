272c53e25b9cb794324ab36693a431352bdd4e19
ValeriiaM <moskalenkovvy@gmail.com>
2026-02-26 18:31:26 +0200

    fix size

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -7,7 +7,7 @@ interface LogoProps {
 const Logo = ({ variant = 'dark' }: LogoProps) => {
   return (
     <div className={`${css.logo} ${css[variant]}`}>
-      <svg width='24' height='24'>
+      <svg width='23' height='23'>
         <use href='/logo.svg'></use>
       </svg>
       <span>Подорожники</span>
