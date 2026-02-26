92753c3059c7abe9701c0b6127a1a7713fdabe34
ValeriiaM <moskalenkovvy@gmail.com>
2026-02-26 12:13:24 +0200

    add socials svg

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -2,6 +2,7 @@ import Link from '../Link/Link';
 import css from './Footer.module.css';
 
 const Footer = () => {
+  const svgSize = 24;
   return (
     <footer className={css.footer_section}>
       <div className={css.container}>
@@ -15,22 +16,30 @@ const Footer = () => {
             <ul className={css.socials}>
               <li>
                 <Link href='https://www.facebook.com/' variant='link'>
-                  F
+                  <svg width={svgSize} height={svgSize}>
+                    <use href='/icons/sprite.svg#icon-Facebook' />
+                  </svg>
                 </Link>
               </li>
               <li>
                 <Link href='https://www.instagram.com/' variant='link'>
-                  I
+                  <svg width={svgSize} height={svgSize}>
+                    <use href='/icons/sprite.svg#icon-Instagram' />
+                  </svg>
                 </Link>
               </li>
               <li>
                 <Link href='https://x.com/' variant='link'>
-                  X
+                  <svg width={svgSize} height={svgSize}>
+                    <use href='/icons/sprite.svg#icon-X' />
+                  </svg>
                 </Link>
               </li>
               <li>
                 <Link href='https://www.youtube.com/' variant='link'>
-                  Y
+                  <svg width={svgSize} height={svgSize}>
+                    <use href='/icons/sprite.svg#icon-Youtube' />
+                  </svg>
                 </Link>
               </li>
             </ul>
