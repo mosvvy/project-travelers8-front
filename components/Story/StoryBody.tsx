d027eb93595f8ef91c7dfbe01c770583b6bbbb0c
Liliia Dudnyk <91532622+LiliiaDud@users.noreply.github.com>
2026-02-27 22:09:12 +0100

    Add draft story component

---
+++ components/Story/StoryBody.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { Story } from '@/app/lib/api/types/stories';
+import css from './Story.module.css';
+import Button from '../Button/Button';
+
+const AddToFavorites = ({ storyId }: { storyId: string }) => {
+  return (
+    <div className={css.favoritesBox}>
+      <div className={css.innerFavoritesBox}>
+        <h3>Збережіть собі історію</h3>
+        <p>Вона буде доступна у вашому профілі у розділі збережене</p>
+        <div className={css.buttonContainer}>
+          <Button
+            fullWidth={true}
+            variant='primary'
+            onClick={() => alert(`Story ${storyId} added to favorites!`)}
+          >
+            Зберегти
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const StoryBody = ({ article, _id }: Pick<Story, 'article' | '_id'>) => {
+  return (
+    <div className={css.articleContainer}>
+      <p className={css.article}>{article}</p>
+      <AddToFavorites storyId={_id} />
+    </div>
+  );
+};
+
+export default StoryBody;
