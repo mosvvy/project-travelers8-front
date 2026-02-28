83a14246351867a6c75c0c4e5dead5c6f697be5a
Liliia Dudnyk <91532622+LiliiaDud@users.noreply.github.com>
2026-02-28 17:49:25 +0100

    Add single story page

--- app/stories/[storyId]/page.tsx
+++ app/stories/[storyId]/page.tsx
@@ -1,9 +1,10 @@
-import css from './page.module.css';
+import { fetchStory } from '@/app/lib/api/clientApi';
+import Section from '@/components/Section/Section';
+import Story from '@/components/Story/Story';
 
-export default function StoryPage() {
-  return (
-    <>
-      <h1>Історія</h1>
-    </>
-  );
+export default async function StoryPage({ params }: { params: Promise<{ storyId: string }> }) {
+  const { storyId } = await params;
+  const story = await fetchStory(storyId);
+
+  return <Story story={story} />;
 }
