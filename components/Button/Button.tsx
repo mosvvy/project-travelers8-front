37c006058e94b2907026d8bd89137f9b4c8a0452
Liliia Dudnyk <91532622+LiliiaDud@users.noreply.github.com>
2026-02-28 17:23:03 +0100

    Allow to stretch button full width

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -5,16 +5,19 @@ import css from './Button.module.css';
 interface ButtonProps {
   variant?: 'primary' | 'secondary';
   children: React.ReactNode;
+  fullWidth?: boolean;
   onClick: () => void;
 }
 
-const Button = ({ variant = 'primary', children, onClick }: ButtonProps) => {
+const Button = ({ variant = 'primary', children, fullWidth = false, onClick }: ButtonProps) => {
   const handleClick = () => {
     onClick();
   };
 
+  const widthClass = fullWidth ? css.fullWidth : css.defaultWidth;
+
   return (
-    <button className={clsx(css.button, variant && css[variant])} onClick={handleClick}>
+    <button className={clsx(css.button, widthClass, variant && css[variant])} onClick={handleClick}>
       {children}
     </button>
   );
