433ed1e5a8231df239f206b300d8acd66410eeb1
ValeriiaM <moskalenkovvy@gmail.com>
2026-03-01 19:49:05 +0200

    isAuthenticatedadd and use

--- app/lib/store/authStore.ts
+++ app/lib/store/authStore.ts
@@ -2,13 +2,15 @@ import { create } from 'zustand';
 import type { AuthUser } from '../api/clientApi';
 
 type AuthState = {
+  isAuthenticated: boolean;
   user: AuthUser | null;
   setUser: (user: AuthUser) => void;
   clearUser: () => void;
 };
 
 export const useAuthStore = create<AuthState>(set => ({
+  isAuthenticated: false,
   user: null,
-  setUser: user => set({ user }),
-  clearUser: () => set({ user: null }),
+  setUser: user => set({ user, isAuthenticated: true }),
+  clearUser: () => set({ user: null, isAuthenticated: false }),
 }));
