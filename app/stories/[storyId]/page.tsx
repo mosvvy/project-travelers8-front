bbc17ef05c49c273139ca779bd62a33159a8f656
Liliia Dudnyk <91532622+LiliiaDud@users.noreply.github.com>
2026-03-01 14:05:50 +0100

    Wrap Story component into Section on single story page

--- app/stories/[storyId]/page.tsx
+++ app/stories/[storyId]/page.tsx
@@ -1,10 +1,15 @@
 import { fetchStory } from '@/app/lib/api/clientApi';
 import Section from '@/components/Section/Section';
 import Story from '@/components/Story/Story';
+import css from './page.module.css';
 
 export default async function StoryPage({ params }: { params: Promise<{ storyId: string }> }) {
   const { storyId } = await params;
   const story = await fetchStory(storyId);
 
-  return <Story story={story} />;
+  return (
+    <Section sectionClassName={css.storyContainer} containerClassName={css.storyInnerContainer}>
+      <Story story={story} />
+    </Section>
+  );
 }
