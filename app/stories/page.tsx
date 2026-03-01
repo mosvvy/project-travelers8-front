0417117e7f7596e83f9bd8bcf6390192a5b1f428
dmitbykanov <dmitbykanov@gmail.com>
2026-03-01 18:40:06 +0200

    delete code

--- app/stories/page.tsx
+++ app/stories/page.tsx
@@ -1,19 +1,6 @@
 import css from './page.module.css';
-import { fetchStories } from '../lib/api/serverApi';
-import { StoriesResponse } from '../lib/api/clientApi';
-import Image from 'next/image';
 
-interface Props {
-  searchParams: Promise<{ page?: string; category?: string }>;
-}
-
-export default async function StoriesPage({ searchParams }: Props) {
-  const { page = '1', category = 'All' } = await searchParams;
-
-  const data = (await fetchStories({ page: Number(page), category })) || { stories: [] };
-
-  const stories = data?.stories ?? [];
-
+export default async function StoriesPage() {
   return (
     <>
       <h1 className={css.title}>Історії Мандрівників</h1>
