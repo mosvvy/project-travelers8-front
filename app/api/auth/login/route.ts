4d78d1f6e8c2416b9ae88740ece668f7bf5f05ff
Liliia Dudnyk <91532622+LiliiaDud@users.noreply.github.com>
2026-03-02 21:04:50 +0100

    Wait until async setCookies completed in login

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -8,7 +8,7 @@ export async function POST(req: NextRequest) {
     const body = await req.json();
     const apiRes = await api.post('auth/login', body);
 
-    setCookies(apiRes.headers['set-cookie']);
+    await setCookies(apiRes.headers['set-cookie']);
 
     return NextResponse.json(apiRes.data, {
       status: apiRes.status,
