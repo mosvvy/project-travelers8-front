bc46b101f3a8f10802e2553cf5746653b5c8e5e5
Liliia Dudnyk <91532622+LiliiaDud@users.noreply.github.com>
2026-03-02 21:51:59 +0100

    Use proxy logout in logout button

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -7,19 +7,22 @@ import clsx from 'clsx';
 import css from './Header.module.css';
 import BurgerMenu from './BurgerMenu/BurgerMenu';
 import { useAuthStore } from '@/app/lib/store/authStore';
+import { logout } from '@/app/lib/api/proxyApi';
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const pathname = usePathname();
   const isHomePage = pathname === '/';
-  
+
   const { user, isAuthenticated, clearUser } = useAuthStore();
 
   const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
   const closeMenu = () => setIsMenuOpen(false);
 
-  const handleLogout = () => {
-    clearUser();
+  const handleLogout = async () => {
+    if (await logout()) {
+      clearUser();
+    }
   };
 
   useEffect(() => {
@@ -51,16 +54,16 @@ const Header = () => {
   return (
     <header className={clsx(css.header, isHomePage && css.heroHeader)}>
       <div className={css.container}>
-        <Link href="/" className={css.logo}>
-          <svg width="23" height="23" viewBox="0 0 23 23" fill="currentColor">
-            <use href="/logo.svg#icon-logo" />
+        <Link href='/' className={css.logo}>
+          <svg width='23' height='23' viewBox='0 0 23 23' fill='currentColor'>
+            <use href='/logo.svg#icon-logo' />
           </svg>
           <span>Подорожники</span>
         </Link>
 
         <div className={css.headerRight}>
           <nav className={css.desktopNav}>
-            {navigation.map((item) => (
+            {navigation.map(item => (
               <Link
                 key={item.href}
                 href={item.href}
@@ -73,37 +76,41 @@ const Header = () => {
 
           {isAuthenticated ? (
             <div className={css.userSection}>
-              <Link href="/stories/create" className={css.publishBtnDesktop}>
+              <Link href='/stories/create' className={css.publishBtnDesktop}>
                 Опублікувати історію
               </Link>
               <div className={css.profile}>
-                <img 
-                  src={user?.avatarUrl || '/avatarDefault.jpg'} 
-                  alt={user?.name} 
+                <img
+                  src={user?.avatarUrl || '/avatarDefault.jpg'}
+                  alt={user?.name}
                   className={css.avatar}
                 />
                 <span className={css.userName}>{user?.name}</span>
               </div>
               <button onClick={handleLogout} className={css.logoutBtn}>
-                <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
-                  <path d="M16 13v-2H7V8l-5 4 5 4v-3h9zM20 3h-9c-1.1 0-2 .9-2 2v4h2V5h9v14h-9v-4H9v4c0 1.1.9 2 2 2h9c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z"/>
+                <svg width='24' height='24' viewBox='0 0 24 24' fill='currentColor'>
+                  <path d='M16 13v-2H7V8l-5 4 5 4v-3h9zM20 3h-9c-1.1 0-2 .9-2 2v4h2V5h9v14h-9v-4H9v4c0 1.1.9 2 2 2h9c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z' />
                 </svg>
               </button>
             </div>
           ) : (
             <div className={css.authLinks}>
-              <Link href="/auth/login" className={css.btnLogin}>Вхід</Link>
-              <Link href="/auth/register" className={css.btnRegister}>Реєстрація</Link>
+              <Link href='/auth/login' className={css.btnLogin}>
+                Вхід
+              </Link>
+              <Link href='/auth/register' className={css.btnRegister}>
+                Реєстрація
+              </Link>
             </div>
           )}
         </div>
 
         {/* Планшетна кнопка - ДЛЯ ВСІХ! */}
-        <Link href="/stories/create" className={css.publishBtnTablet}>
+        <Link href='/stories/create' className={css.publishBtnTablet}>
           Опублікувати історію
         </Link>
 
-        <button className={css.burgerBtn} onClick={toggleMenu} aria-label="Меню">
+        <button className={css.burgerBtn} onClick={toggleMenu} aria-label='Меню'>
           <span className={css.burgerIcon}></span>
         </button>
       </div>
