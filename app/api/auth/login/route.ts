6d6ecfb1ec46cc14beb908df64d87ff1e488f7be
Liliia Dudnyk <91532622+LiliiaDud@users.noreply.github.com>
2026-03-02 18:24:59 +0100

    Move cookie persisting logic into utils helper

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,37 +1,14 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { api } from '../../api';
-import { cookies } from 'next/headers';
 import { isAxiosError } from 'axios';
-import { parse } from 'cookie';
+import { setCookies } from '../../_utils/utils';
 
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
     const apiRes = await api.post('auth/login', body);
 
-    const cookieStore = await cookies();
-    const setCookie = apiRes.headers['set-cookie'];
-
-    if (setCookie) {
-      const cookieArray = Array.isArray(setCookie) ? setCookie : [setCookie];
-
-      for (const cookieStr of cookieArray) {
-        const [nameValue] = cookieStr.split(';');
-        const [name, value] = nameValue.split('=');
-        const parsedCookies = parse(cookieStr);
-        //httpOnly: true та sameSite: 'lax' захищає токени від XSS атак
-        cookieStore.set({
-          name,
-          value,
-          httpOnly: true,
-          secure: process.env.NODE_ENV === 'production',
-          sameSite: 'lax',
-          path: parsedCookies.Path || '/',
-          maxAge: Number(parsedCookies['Max-Age']),
-          expires: parsedCookies.Expires ? new Date(parsedCookies.Expires) : undefined,
-        });
-      }
-    }
+    setCookies(apiRes.headers['set-cookie']);
 
     return NextResponse.json(apiRes.data, {
       status: apiRes.status,
