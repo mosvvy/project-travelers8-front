17a37502e0e4109619c64afc65883969562f8a65
ValeriiaM <moskalenkovvy@gmail.com>
2026-03-04 11:52:14 +0200

    add link to logo

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -7,13 +7,13 @@ import clsx from 'clsx';
 import css from './Header.module.css';
 import BurgerMenu from './BurgerMenu/BurgerMenu';
 import { useAuthStore } from '@/app/lib/store/authStore';
-import Logo from '../Logo/Logo'; 
+import Logo from '../Logo/Logo';
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const pathname = usePathname();
   const isHomePage = pathname === '/';
-  
+
   const { user, isAuthenticated, clearUser } = useAuthStore();
 
   const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
@@ -53,11 +53,13 @@ const Header = () => {
     <header className={clsx(css.header, isHomePage && css.heroHeader)}>
       <div className={css.container}>
         {}
-        <Logo variant={isHomePage ? 'light' : 'dark'} />
+        <Link href='/' className={css.logo}>
+          <Logo variant={isHomePage ? 'light' : 'dark'} />
+        </Link>
 
         <div className={css.headerRight}>
           <nav className={css.desktopNav}>
-            {navigation.map((item) => (
+            {navigation.map(item => (
               <Link
                 key={item.href}
                 href={item.href}
@@ -70,37 +72,41 @@ const Header = () => {
 
           {isAuthenticated ? (
             <div className={css.userSection}>
-              <Link href="/stories/create" className={css.publishBtnDesktop}>
+              <Link href='/stories/create' className={css.publishBtnDesktop}>
                 Опублікувати історію
               </Link>
               <div className={css.profile}>
-                <img 
-                  src={user?.avatarUrl || '/avatarDefault.jpg'} 
-                  alt={user?.name} 
+                <img
+                  src={user?.avatarUrl || '/avatarDefault.jpg'}
+                  alt={user?.name}
                   className={css.avatar}
                 />
                 <span className={css.userName}>{user?.name}</span>
               </div>
               <button onClick={handleLogout} className={css.logoutBtn}>
-                <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
-                  <path d="M16 13v-2H7V8l-5 4 5 4v-3h9zM20 3h-9c-1.1 0-2 .9-2 2v4h2V5h9v14h-9v-4H9v4c0 1.1.9 2 2 2h9c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z"/>
+                <svg width='24' height='24' viewBox='0 0 24 24' fill='currentColor'>
+                  <path d='M16 13v-2H7V8l-5 4 5 4v-3h9zM20 3h-9c-1.1 0-2 .9-2 2v4h2V5h9v14h-9v-4H9v4c0 1.1.9 2 2 2h9c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z' />
                 </svg>
               </button>
             </div>
           ) : (
             <div className={css.authLinks}>
-              <Link href="/auth/login" className={css.btnLogin}>Вхід</Link>
-              <Link href="/auth/register" className={css.btnRegister}>Реєстрація</Link>
+              <Link href='/auth/login' className={css.btnLogin}>
+                Вхід
+              </Link>
+              <Link href='/auth/register' className={css.btnRegister}>
+                Реєстрація
+              </Link>
             </div>
           )}
         </div>
 
         {}
-        <Link href="/stories/create" className={css.publishBtnTablet}>
+        <Link href='/stories/create' className={css.publishBtnTablet}>
           Опублікувати історію
         </Link>
 
-        <button className={css.burgerBtn} onClick={toggleMenu} aria-label="Меню">
+        <button className={css.burgerBtn} onClick={toggleMenu} aria-label='Меню'>
           <span className={css.burgerIcon}></span>
         </button>
       </div>
