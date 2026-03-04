b89719afce408ef8e3b380c21cbce0730b880481
ValeriiaM <moskalenkovvy@gmail.com>
2026-03-04 11:39:57 +0200

    add Story

--- types/story.ts
+++ types/story.ts
@@ -62,3 +62,24 @@ export interface CreateStoryResponse {
   message: string;
   data: IStory;
 }
+
+export interface Story {
+  id: string;
+  img: string;
+  title: string;
+  article: string;
+  category: {
+    id: string;
+    name: string;
+  };
+
+  ownerId: {
+    id: string;
+    name: string;
+    avatarUrl: string;
+  };
+  date: string;
+
+  favoriteCount: number;
+  isFavorite?: boolean;
+}
