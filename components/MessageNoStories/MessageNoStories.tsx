fb65bf38a3bba958d2c5ccae8b279b146ae0aa2d
Ivan Shakhov <ivanshahov04@gmail.com>
2026-03-05 14:36:15 +0200

    fix

--- components/MessageNoStories/MessageNoStories.tsx
+++ components/MessageNoStories/MessageNoStories.tsx
@@ -4,16 +4,19 @@ import Link from '../Link/Link';
 type PageProps = {
   text: string;
   buttonText: string;
-  route: string;
+  route?: string;
 };
 
 const MessageNoStories = ({ text, buttonText, route }: PageProps) => {
   return (
     <div className={css.messageWrapper}>
       <p className={css.messageText}>{text}</p>
-      <Link href={route} className={css.messageButtonText}>
-        {buttonText}
-      </Link>
+
+      {route && (
+        <Link href={route} className={css.messageButtonText}>
+          {buttonText}
+        </Link>
+      )}
     </div>
   );
 };
